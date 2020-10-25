d03b416382342de67886e478d0f404f1c4a69fb9
r0man-x <snegovata@gmail.com>
2020-10-25 21:47:39 +0300

    Update index.js
    task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function check(str, bracketsConfig) {
+    
+  let bracketsObject = bracketsConfig.reduce(function(acc, cur) {
+    acc[cur[0]] = cur[1];
+    return acc;
+  }, {})
+
+  let stack = []
+
+  for(let bracket of str) {
+    let bracketCanBeClosing = Object.values(bracketsObject).indexOf(bracket) > -1
+
+    if (bracketCanBeClosing) {
+      if (stack.length !== 0 && stack[stack.length-1] === bracket) {
+        stack.pop()
+      } else {
+        stack.push(bracketsObject[bracket])
+      }
+    } else {
+      stack.push(bracketsObject[bracket])
+    }
+  }
+
+  return stack.length === 0
   // your solution
 }
